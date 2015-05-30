d659af6b45779e07340ad62dd4b8f9929198f263
Odepax <odepaxforuma@gmail.com>
2015-05-30 15:48:38 +0200

    Mise en place des composants.
     - Ajout des composants de la sidebar;
     - Ajout de méthode utilitaires;
     - Liaison de données avec les comboboxes de la sidebar;
     - Ajout de lignes fictives aux combos pour simuler la sélection de toutes
       les options plus tard.
    
    Reste à faire :
    
     - Gestion évènementielle;
     - Bouton "Détail des offres" dans la sidebar.

--- Drakkair/FormMain.Designer.cs
+++ Drakkair/FormMain.Designer.cs
@@ -31,13 +31,13 @@
 			this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
 			this.panelTopBar = new System.Windows.Forms.Panel();
 			this.buttonAdmin = new System.Windows.Forms.Button();
-			this.dataGridView1 = new System.Windows.Forms.DataGridView();
-			this.panelSideBar = new System.Windows.Forms.FlowLayoutPanel();
 			this.pictureLogo = new System.Windows.Forms.PictureBox();
+			this.dataGridView = new System.Windows.Forms.DataGridView();
+			this.panelSideBar = new System.Windows.Forms.FlowLayoutPanel();
 			this.tableLayoutPanel1.SuspendLayout();
 			this.panelTopBar.SuspendLayout();
-			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
 			((System.ComponentModel.ISupportInitialize)(this.pictureLogo)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
 			this.SuspendLayout();
 			// 
 			// tableLayoutPanel1
@@ -50,7 +50,7 @@
 			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
 			this.tableLayoutPanel1.Controls.Add(this.panelTopBar, 1, 0);
 			this.tableLayoutPanel1.Controls.Add(this.pictureLogo, 0, 0);
-			this.tableLayoutPanel1.Controls.Add(this.dataGridView1, 1, 1);
+			this.tableLayoutPanel1.Controls.Add(this.dataGridView, 1, 1);
 			this.tableLayoutPanel1.Controls.Add(this.panelSideBar, 0, 1);
 			this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
 			this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(4);
@@ -58,6 +58,7 @@
 			this.tableLayoutPanel1.RowCount = 2;
 			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 150F));
 			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
 			this.tableLayoutPanel1.Size = new System.Drawing.Size(1002, 665);
 			this.tableLayoutPanel1.TabIndex = 0;
 			// 
@@ -80,28 +81,39 @@
 			this.buttonAdmin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(82)))), ((int)(((byte)(126)))), ((int)(((byte)(53)))));
 			this.buttonAdmin.FlatAppearance.BorderSize = 0;
 			this.buttonAdmin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-			this.buttonAdmin.Location = new System.Drawing.Point(525, 87);
+			this.buttonAdmin.Location = new System.Drawing.Point(525, 84);
 			this.buttonAdmin.Margin = new System.Windows.Forms.Padding(20, 20, 20, 30);
 			this.buttonAdmin.Name = "buttonAdmin";
-			this.buttonAdmin.Size = new System.Drawing.Size(94, 33);
+			this.buttonAdmin.Size = new System.Drawing.Size(94, 36);
 			this.buttonAdmin.TabIndex = 1;
 			this.buttonAdmin.Text = "ADMIN";
 			this.buttonAdmin.UseVisualStyleBackColor = false;
 			// 
-			// dataGridView1
+			// pictureLogo
 			// 
-			this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+			this.pictureLogo.Image = global::Drakkair.Properties.Resources.logo;
+			this.pictureLogo.Location = new System.Drawing.Point(0, 0);
+			this.pictureLogo.Margin = new System.Windows.Forms.Padding(0);
+			this.pictureLogo.Name = "pictureLogo";
+			this.pictureLogo.Size = new System.Drawing.Size(350, 150);
+			this.pictureLogo.TabIndex = 3;
+			this.pictureLogo.TabStop = false;
+			// 
+			// dataGridView
+			// 
+			this.dataGridView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-			this.dataGridView1.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(51)))));
-			this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
-			this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-			this.dataGridView1.Location = new System.Drawing.Point(350, 150);
-			this.dataGridView1.Margin = new System.Windows.Forms.Padding(0);
-			this.dataGridView1.Name = "dataGridView1";
-			this.dataGridView1.RowTemplate.Height = 24;
-			this.dataGridView1.Size = new System.Drawing.Size(652, 515);
-			this.dataGridView1.TabIndex = 4;
+			this.dataGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+			this.dataGridView.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(51)))));
+			this.dataGridView.BorderStyle = System.Windows.Forms.BorderStyle.None;
+			this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridView.Location = new System.Drawing.Point(350, 150);
+			this.dataGridView.Margin = new System.Windows.Forms.Padding(0);
+			this.dataGridView.Name = "dataGridView";
+			this.dataGridView.RowTemplate.Height = 24;
+			this.dataGridView.Size = new System.Drawing.Size(652, 515);
+			this.dataGridView.TabIndex = 4;
 			// 
 			// panelSideBar
 			// 
@@ -115,19 +127,9 @@
 			this.panelSideBar.Size = new System.Drawing.Size(350, 515);
 			this.panelSideBar.TabIndex = 5;
 			// 
-			// pictureLogo
-			// 
-			this.pictureLogo.Image = global::Drakkair.Properties.Resources.logo;
-			this.pictureLogo.Location = new System.Drawing.Point(0, 0);
-			this.pictureLogo.Margin = new System.Windows.Forms.Padding(0);
-			this.pictureLogo.Name = "pictureLogo";
-			this.pictureLogo.Size = new System.Drawing.Size(350, 150);
-			this.pictureLogo.TabIndex = 3;
-			this.pictureLogo.TabStop = false;
-			// 
 			// FormMain
 			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 19F);
+			this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 24F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(51)))));
 			this.ClientSize = new System.Drawing.Size(998, 664);
@@ -140,8 +142,8 @@
 			this.Load += new System.EventHandler(this.FormMain_Load);
 			this.tableLayoutPanel1.ResumeLayout(false);
 			this.panelTopBar.ResumeLayout(false);
-			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
 			((System.ComponentModel.ISupportInitialize)(this.pictureLogo)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
 			this.ResumeLayout(false);
 
 		}
@@ -152,7 +154,7 @@
         private System.Windows.Forms.Panel panelTopBar;
 		private System.Windows.Forms.Button buttonAdmin;
 		private System.Windows.Forms.PictureBox pictureLogo;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView;
 		private System.Windows.Forms.FlowLayoutPanel panelSideBar;
 	}
 }
--- Drakkair/FormMain.cs
+++ Drakkair/FormMain.cs
@@ -25,13 +25,282 @@ namespace Drakkair
 		private DataSet DATA = new DataSet();
 		// ----------------------------------------------------------------
 
+		private ComboBox comboDepart = new ComboBox();
+		private ComboBox comboTheme = new ComboBox();
+		private ComboBox comboPension = new ComboBox();
+		// ----------------------------------------------------------------
+
+		private UserTextBox textPrixMin = new UserTextBox();
+		private UserTextBox textPrixMax = new UserTextBox();
+		// ----------------------------------------------------------------
+
 		/// <summary>
-		/// Initialise les composants.
-		/// Va chercher les données dans la BDD.
+		/// Génère automatiquement des boutons vers les catégories de voyage.
 		/// </summary>
+		private void InitSideBarCategoryButtons()
+		{
+			LinkButton buttonTmp;
+			foreach(DataRow line in DATA.Tables["Thematiques"].Rows)
+			{
+				buttonTmp = new LinkButton();
+
+				buttonTmp.Size = new System.Drawing.Size(350, 36);
+				buttonTmp.Tag  = line["id"];
+				buttonTmp.Text = (string)line["name"];
+				buttonTmp.TextAlign = ContentAlignment.MiddleRight;
+
+				panelSideBar.Controls.Add(buttonTmp);
+			}
+		}
+		// ----------------------------------------------------------------
+
+		/// <summary>
+		/// Initialise la barre latérale avec tous ses composants :
+		/// - les labels de légende
+		/// - les textbox de prix
+		/// - les combos départ, theme et pension
+		/// - les boutons des catégories d'offres
+		/// </summary>
+		private void InitSideBar()
+		{
+			Label labelTmp;
+
+			// titre catégories
+			labelTmp = new Label();
+			labelTmp.Text = "Catégories d'offres";
+			labelTmp.Font = new System.Drawing.Font("Open Sans", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			labelTmp.Margin = new System.Windows.Forms.Padding(6);
+			labelTmp.Size = new System.Drawing.Size(338, 36);
+			panelSideBar.Controls.Add(labelTmp);
+
+			// génération des boutons des catégories
+			InitSideBarCategoryButtons();
+
+			// titre recherche
+			labelTmp = new Label();
+			labelTmp.Text = "Critères de recherche";
+			labelTmp.Font = new System.Drawing.Font("Open Sans", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			labelTmp.Margin = new System.Windows.Forms.Padding(6);
+			labelTmp.Size = new System.Drawing.Size(338, 36);
+			panelSideBar.Controls.Add(labelTmp);
+
+			// label départ
+			labelTmp = new Label();
+			labelTmp.Text = "Départ";
+			labelTmp.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+			labelTmp.AutoSize = true;
+			labelTmp.Margin = new System.Windows.Forms.Padding(6);
+			panelSideBar.Controls.Add(labelTmp);
+
+			// comboDepart
+			this.comboDepart.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+			this.comboDepart.BackColor = System.Drawing.Color.White;
+			this.comboDepart.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.comboDepart.ForeColor = System.Drawing.Color.Black;
+			this.comboDepart.FormattingEnabled = true;
+			this.comboDepart.Margin = new System.Windows.Forms.Padding(6);
+			this.comboDepart.Size = new System.Drawing.Size(248, 32);
+			panelSideBar.Controls.Add(this.comboDepart);
+
+			// label theme
+			labelTmp = new Label();
+			labelTmp.Text = "Theme";
+			labelTmp.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+			labelTmp.AutoSize = true;
+			labelTmp.Margin = new System.Windows.Forms.Padding(6);
+			panelSideBar.Controls.Add(labelTmp);
+
+			// comboTheme
+			this.comboTheme.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+			this.comboTheme.BackColor = System.Drawing.Color.White;
+			this.comboTheme.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.comboTheme.ForeColor = System.Drawing.Color.Black;
+			this.comboTheme.FormattingEnabled = true;
+			this.comboTheme.Location = new System.Drawing.Point(93, 145);
+			this.comboTheme.Margin = new System.Windows.Forms.Padding(6);
+			this.comboTheme.Size = new System.Drawing.Size(248, 32);
+			panelSideBar.Controls.Add(this.comboTheme);
+
+			// label pension
+			labelTmp = new Label();
+			labelTmp.Text = "Pension";
+			labelTmp.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+			labelTmp.AutoSize = true;
+			labelTmp.Location = new System.Drawing.Point(6, 192);
+			labelTmp.Margin = new System.Windows.Forms.Padding(6);
+			labelTmp.Size = new System.Drawing.Size(83, 26);
+			panelSideBar.Controls.Add(labelTmp);
+
+			// comboPension
+			this.comboPension.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+			this.comboPension.BackColor = System.Drawing.Color.White;
+			this.comboPension.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.comboPension.ForeColor = System.Drawing.Color.Black;
+			this.comboPension.FormattingEnabled = true;
+			this.comboPension.Location = new System.Drawing.Point(101, 189);
+			this.comboPension.Margin = new System.Windows.Forms.Padding(6);
+			this.comboPension.Size = new System.Drawing.Size(240, 32);
+			panelSideBar.Controls.Add(this.comboPension);
+
+			// label gamme de prix
+			labelTmp = new Label();
+			labelTmp.Text = "Gamme de prix";
+			labelTmp.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+			labelTmp.Margin = new System.Windows.Forms.Padding(6);
+			labelTmp.Size = new System.Drawing.Size(338, 26);
+			panelSideBar.Controls.Add(labelTmp);
+
+			// sous label gamme de prix
+			labelTmp = new Label();
+			labelTmp.Text = "De";
+			labelTmp.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+			labelTmp.AutoSize = true;
+			labelTmp.Margin = new System.Windows.Forms.Padding(6);
+			panelSideBar.Controls.Add(labelTmp);
+
+			// textPrixMin
+			this.textPrixMin.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+			this.textPrixMin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(82)))), ((int)(((byte)(126)))), ((int)(((byte)(53)))));
+			this.textPrixMin.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.textPrixMin.ForeColor = System.Drawing.Color.White;
+			this.textPrixMin.Size = new System.Drawing.Size(86, 32);
+			panelSideBar.Controls.Add(this.textPrixMin);
+
+			// sous label gamme de prix
+			labelTmp = new Label();
+			labelTmp.Text = "à";
+			labelTmp.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+			labelTmp.AutoSize = true;
+			labelTmp.Margin = new System.Windows.Forms.Padding(6);
+			panelSideBar.Controls.Add(labelTmp);
+
+			// textPrixMax
+			this.textPrixMax.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+			this.textPrixMax.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(82)))), ((int)(((byte)(126)))), ((int)(((byte)(53)))));
+			this.textPrixMax.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.textPrixMax.ForeColor = System.Drawing.Color.White;
+			this.textPrixMax.Size = new System.Drawing.Size(118, 32);
+			panelSideBar.Controls.Add(this.textPrixMax);
+
+			// sous label gamme de prix
+			labelTmp = new Label();
+			labelTmp.Text = "€";
+			labelTmp.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+			labelTmp.AutoSize = true;
+			labelTmp.Margin = new System.Windows.Forms.Padding(6);
+			panelSideBar.Controls.Add(labelTmp);
+		}
+		// ----------------------------------------------------------------
+
+		/// <summary>
+		/// Rmplit le dataset this.DATA avec les données souhaitées.
+		/// </summary>
+		private void RetrieveData()
+		{
+			new OleDbDataAdapter(@"
+				SELECT codeVille AS id, nomVille AS name
+				FROM tblVilles
+			", DB).Fill(DATA, "Departs");
+
+			new OleDbDataAdapter(@"
+				SELECT codeThem AS id, libThem AS name
+				FROM tblThematique
+			", DB).Fill(DATA, "Thematiques");
+
+			new OleDbDataAdapter(@"
+				SELECT NumCategorie AS id, NomCategorie AS name
+				FROM tblHebergement
+			", DB).Fill(DATA, "Pensions");
+		}
+		// ----------------------------------------------------------------
+
 		public FormMain()
 		{
 			InitializeComponent();
+		}
+		// ----------------------------------------------------------------
+
+		/// <summary>
+		/// Fait une liason de données sur une combobox avec une table de this.DATA.
+		/// </summary>
+		/// <param name="combo">La combocob à lier.</param>
+		/// <param name="tableName">Le nom de la table dans laquelle aller chercher les données.</param>
+		/// <param name="valueMember">Valeur pour la propriété combo.ValueMember.</param>
+		/// <param name="displayMemeber">Valeur pour la propriété combo.DisplayMember.</param>
+		private void ComboDataLink(ComboBox combo, string tableName, string valueMember, string displayMemeber)
+		{
+			combo.DataSource = DATA.Tables[tableName];
+			combo.ValueMember = valueMember;
+			combo.DisplayMember = displayMemeber;
+		}
+		// ----------------------------------------------------------------
+
+		/// <summary>
+		/// Ajoute une ligne dans une table de this.DATA.
+		/// </summary>
+		/// <param name="tableName">Le nom de la table à modifier.</param>
+		/// <param name="config">
+		/// Tableau de configuration pour la ligne à rajouter.
+		/// Chaque sous tableau a deux dimensions :
+		/// - la première case est une string qui sert de nom de colonne
+		/// - la seconde sert de valeur dans la nouvelle ligne.
+		/// </param>
+		/// <example>
+		/// AddRowToTable("Dogs", new object[,]{
+		///		{"id", 12},
+		///		{"name", "Olaf"},
+		///		{"nickname", "Ducon"}
+		///	});
+		/// </example>
+		/// <exception cref="IndexOutOfRangeException">Emit automatiquement si le tableau n'a pas les bonnes dimensions.</exception>
+		private void AddRowToTable(string tableName, object[,] config)
+		{
+			var r = DATA.Tables[tableName].NewRow();
+
+			for(int i = 0, c = config.GetLength(0); i < c; i++)
+				r[(string)config[i, 0]] = config[i, 1];
+
+			DATA.Tables[tableName].Rows.Add(r);
+		}
+		// ----------------------------------------------------------------
+
+		/// <summary>
+		/// - rapatrie les données dans le dataset
+		/// - Initialise les composants de la sidebar
+		/// - lie les données à ces composants
+		/// - lie les évènements à ces composants
+		/// </summary>
+		private void FormMain_Load(object sender, EventArgs e)
+		{
+			// par ici les données !
+			RetrieveData();
+
+			// placement des composants
+			InitSideBar();
+
+			// liaison des données avec les composants
+			ComboDataLink(comboDepart, "Departs", "id", "name");
+			ComboDataLink(comboTheme, "Thematiques", "id", "name");
+			ComboDataLink(comboPension, "Pensions", "id", "name");
+
+			// création de lignes fictives dans les tables pour simuler la sélection de toutes les options.
+			AddRowToTable("Departs", new object[,]{
+				{"id", -1},
+				{"name", "Tous les départs"}
+			});
+			comboDepart.SelectedValue = -1;
+
+			AddRowToTable("Thematiques", new object[,]{
+				{"id", -1},
+				{"name", "Tous les thèmes"}
+			});
+			comboTheme.SelectedValue = -1;
+
+			AddRowToTable("Pensions", new object[,]{
+				{"id", -1},
+				{"name", "Tout type de pension"}
+			});
+			comboPension.SelectedValue = -1;
 		}
 		// ----------------------------------------------------------------
 	}
