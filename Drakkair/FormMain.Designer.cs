bc1ee1f01ba2eca1d7ba59e8242066dccdd3e624
Odepax <odepaxforuma@gmail.com>
2015-06-11 20:19:27 +0200

    Fin du travail. Merge status : READY !
     - Débug du formulaire principal;
     - Débug de la synchro dans Hotels;

--- Drakkair/FormMain.Designer.cs
+++ Drakkair/FormMain.Designer.cs
@@ -31,11 +31,11 @@
 		{
 			this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
 			this.panelTopBar = new System.Windows.Forms.Panel();
-			this.buttonHotels = new Drakkair.Controls.UserButton();
 			this.buttonAdmin = new System.Windows.Forms.Button();
 			this.pictureLogo = new System.Windows.Forms.PictureBox();
 			this.panelSideBar = new System.Windows.Forms.FlowLayoutPanel();
 			this.dataGridView = new System.Windows.Forms.DataGridView();
+			this.buttonHotels = new Drakkair.Controls.UserButton();
 			this.tableLayoutPanel1.SuspendLayout();
 			this.panelTopBar.SuspendLayout();
 			((System.ComponentModel.ISupportInitialize)(this.pictureLogo)).BeginInit();
@@ -61,7 +61,7 @@
 			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 150F));
 			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
 			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-			this.tableLayoutPanel1.Size = new System.Drawing.Size(1008, 718);
+			this.tableLayoutPanel1.Size = new System.Drawing.Size(1106, 761);
 			this.tableLayoutPanel1.TabIndex = 0;
 			// 
 			// panelTopBar
@@ -75,23 +75,8 @@
 			this.panelTopBar.Location = new System.Drawing.Point(350, 0);
 			this.panelTopBar.Margin = new System.Windows.Forms.Padding(0);
 			this.panelTopBar.Name = "panelTopBar";
-			this.panelTopBar.Size = new System.Drawing.Size(658, 150);
+			this.panelTopBar.Size = new System.Drawing.Size(756, 150);
 			this.panelTopBar.TabIndex = 1;
-			// 
-			// buttonHotels
-			// 
-			this.buttonHotels.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-			this.buttonHotels.AutoSize = true;
-			this.buttonHotels.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(82)))), ((int)(((byte)(126)))), ((int)(((byte)(53)))));
-			this.buttonHotels.FlatAppearance.BorderSize = 0;
-			this.buttonHotels.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-			this.buttonHotels.Location = new System.Drawing.Point(394, 84);
-			this.buttonHotels.Margin = new System.Windows.Forms.Padding(20, 20, 20, 30);
-			this.buttonHotels.Name = "buttonHotels";
-			this.buttonHotels.Size = new System.Drawing.Size(97, 36);
-			this.buttonHotels.TabIndex = 2;
-			this.buttonHotels.Text = "HÔTELS";
-			this.buttonHotels.UseVisualStyleBackColor = false;
 			// 
 			// buttonAdmin
 			// 
@@ -99,7 +84,7 @@
 			this.buttonAdmin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(82)))), ((int)(((byte)(126)))), ((int)(((byte)(53)))));
 			this.buttonAdmin.FlatAppearance.BorderSize = 0;
 			this.buttonAdmin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-			this.buttonAdmin.Location = new System.Drawing.Point(531, 84);
+			this.buttonAdmin.Location = new System.Drawing.Point(629, 84);
 			this.buttonAdmin.Margin = new System.Windows.Forms.Padding(20, 20, 20, 30);
 			this.buttonAdmin.Name = "buttonAdmin";
 			this.buttonAdmin.Size = new System.Drawing.Size(94, 36);
@@ -126,11 +111,12 @@
 			this.panelSideBar.Location = new System.Drawing.Point(0, 150);
 			this.panelSideBar.Margin = new System.Windows.Forms.Padding(0);
 			this.panelSideBar.Name = "panelSideBar";
-			this.panelSideBar.Size = new System.Drawing.Size(350, 568);
+			this.panelSideBar.Size = new System.Drawing.Size(350, 611);
 			this.panelSideBar.TabIndex = 5;
 			// 
 			// dataGridView
 			// 
+			this.dataGridView.AllowUserToAddRows = false;
 			this.dataGridView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
@@ -139,18 +125,33 @@
 			this.dataGridView.BorderStyle = System.Windows.Forms.BorderStyle.None;
 			this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
 			this.dataGridView.Location = new System.Drawing.Point(350, 150);
-			this.dataGridView.Margin = new System.Windows.Forms.Padding(0);
+			this.dataGridView.Margin = new System.Windows.Forms.Padding(0, 0, 6, 4);
 			this.dataGridView.Name = "dataGridView";
 			this.dataGridView.RowTemplate.Height = 24;
-			this.dataGridView.Size = new System.Drawing.Size(658, 568);
+			this.dataGridView.Size = new System.Drawing.Size(750, 607);
 			this.dataGridView.TabIndex = 4;
+			// 
+			// buttonHotels
+			// 
+			this.buttonHotels.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.buttonHotels.AutoSize = true;
+			this.buttonHotels.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(82)))), ((int)(((byte)(126)))), ((int)(((byte)(53)))));
+			this.buttonHotels.FlatAppearance.BorderSize = 0;
+			this.buttonHotels.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.buttonHotels.Location = new System.Drawing.Point(492, 84);
+			this.buttonHotels.Margin = new System.Windows.Forms.Padding(20, 20, 20, 30);
+			this.buttonHotels.Name = "buttonHotels";
+			this.buttonHotels.Size = new System.Drawing.Size(97, 36);
+			this.buttonHotels.TabIndex = 2;
+			this.buttonHotels.Text = "HÔTELS";
+			this.buttonHotels.UseVisualStyleBackColor = false;
 			// 
 			// FormMain
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 24F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(51)))));
-			this.ClientSize = new System.Drawing.Size(1004, 717);
+			this.ClientSize = new System.Drawing.Size(1102, 760);
 			this.Controls.Add(this.tableLayoutPanel1);
 			this.Font = new System.Drawing.Font("Open Sans", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
 			this.ForeColor = System.Drawing.Color.White;
